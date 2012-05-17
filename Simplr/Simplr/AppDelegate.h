6aceb0009029c1603d6b1c93fd242f601142c64b
Jordan Nelson <trentottosen777@gmail.com>
2012-05-17 14:52:04 -0500

    Initial creation of Simplr project

---
+++ Simplr/Simplr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Simplr
+//
+//  Created by Jordan Nelson on 5/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
